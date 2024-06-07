9a7e7f4ea5f2b985bdc41a54f312bdc14eba5b05
VBSREELAKSHMI123 <vbsreelakshmi06@gmail.com>
2024-06-07 14:43:05 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import AddStudent from './components/AddStudent';
 import SearchStudent from './components/SearchStudent';
 import DeleteStudent from './components/DeleteStudent';
 import ViewStudent from './components/ViewStudent';
-import { BrowserRouter, Route, Router } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
   <BrowserRouter>
-  <Router>
+  <Routes>
     <Route path='/' element={<AddStudent/>}/>
     <Route path='/search' element={<SearchStudent/>}/>
     <Route path='/delete' element={<DeleteStudent/>}/>
     <Route path='/view' element={<ViewStudent/>}/>
-  </Router>
+  </Routes>
   </BrowserRouter>
   );
 }
